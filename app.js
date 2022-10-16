a7c619e5211261659cec0d26cade858d1d9d7311
Yves62 <boulinguez.yves@orange.fr>
2022-10-16 17:35:56 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,70 @@
+const form = document.querySelector("form");
+const displayCookie = document.querySelector(".display-cookie");
+const inputName = document.querySelector("#name");
+const inputValue = document.querySelector("#value");
+const nameCookie = document.querySelector(".name");
+const valueCookie = document.querySelector(".value");
+const showDisplayCookie = document.querySelector(".show-card-cookie");
+const clearCookie = document.querySelector(".clear-span");
+const toast = document.querySelector(".toast");
+let newCookie;
+const errorMessage = document.querySelector(".display-error");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (!inputName.value && !inputValue.value) {
+    handleError();
+  } else {
+    createCookie(inputName.value, inputValue.value);
+    showPushNotification("green", "Cookie crée");
+  }
+});
+
+showDisplayCookie.addEventListener("click", () => {
+  if (!inputName.value && !inputValue.value) {
+    handleError();
+  } else {
+    displayCookie.style.display = "flex";
+  }
+});
+
+createCookie = (name, value) => {
+  nameCookie.textContent = name;
+  valueCookie.textContent = value;
+  document.cookie = name + "=" + value;
+};
+
+showPushNotification = (color, value) => {
+  toast.style.display = "block";
+  toast.style.backgroundColor = color;
+  toast.style.color = "white";
+  toast.textContent = value;
+  setTimeout(() => {
+    toast.style.display = "none";
+  }, 2000);
+};
+
+clearCookie.addEventListener("click", () => {
+  deleteCookie(inputName.value);
+});
+
+clearInput = () => {
+  inputName.value = "";
+  inputValue.value = "";
+  nameCookie.textContent = "";
+  valueCookie.textContent = "";
+};
+
+deleteCookie = (name) => {
+  document.cookie = `${name} =; Max-Age=0`;
+  clearInput();
+  displayCookie.style.display = "none";
+  showPushNotification("red", "Cookie supprimé");
+};
+
+handleError = () => {
+  errorMessage.style.display = "block";
+  setTimeout(() => {
+    errorMessage.style.display = "none";
+  }, 2000);
+};
